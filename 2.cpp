2fdc2b59cc1f7315868b796f07987460062e243a
Tamara <tamaraavgust14@gmail.com>
2023-09-29 23:52:46 +0700

    Добавление структур

--- 2.cpp
+++ 2.cpp
@@ -3,3 +3,28 @@
 #include <conio.h>
 #include <string.h>
 #include <Windows.h>
+
+struct Passenger {
+	int balance;
+};
+
+struct Car {
+	bool rate;
+	Fuel fuel;
+};
+
+struct Fuel {
+	bool capacity;
+};
+
+struct Driver {
+	Car car;
+	int experience, orderAmount, balance;
+};
+
+struct Order {
+	bool rate, congestion;
+	Driver driver;
+	Passenger passenger;
+	int payment;
+};
