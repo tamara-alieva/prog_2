462c879e2910d5985c89301d1144e4e58187ab0f
Tamara <tamaraavgust14@gmail.com>
2023-09-30 17:19:07 +0700

    Добавление функции output_passenger

--- 2.cpp
+++ 2.cpp
@@ -4,28 +4,34 @@
 #include <string.h>
 #include <Windows.h>
 void input_passenger(Passenger person);
+void output_passenger(Passenger person);
 
+// Пассажир
 typedef struct {
 	char name[20];
 	int balance;
 } Passenger;
 
+// Автомобиль
 typedef struct {
 	char brand[20];
 	bool rate;
 	Fuel fuel;
 } Car;
 
+// Топливный бак
 typedef struct {
 	bool capacity;
 } Fuel;
 
+// Водитель
 typedef struct {
 	char name[20];
 	Car car;
 	int experience, orderAmount, balance;
 } Driver;
 
+// Заказ
 typedef struct {
 	bool rate, congestion;
 	Driver driver;
@@ -33,6 +39,7 @@ typedef struct {
 	int payment;
 } Order;
 
+// Ввод данных о пассажире
 void input_passenger(Passenger person) {
 	printf("** Ввод данных пассажира **\n");
 	do {
@@ -46,4 +53,13 @@ void input_passenger(Passenger person) {
 			printf("Ошибка. Введите баланс: ");
 		}
 	} while (person.balance < 0);
+	printf("Данные успешно введены!\n\n");
+}
+
+// Вывод данных о пассажире
+void output_passenger(Passenger person) {
+	if (person.name[0] == NULL || person.balance < 0)
+		printf("Данные о пассажире отсутствуют!");
+	else
+		printf("Данные о пассажире:\n-Имя: %s\n-Баланс: %d\n\n", person.name, person.balance);
 }
