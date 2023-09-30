03aab1fde8ca2b0c3b24465bf3d8c44e0447ac11
Tamara <tamaraavgust14@gmail.com>
2023-09-30 21:30:38 +0700

    Объявление и определение функций для Passenger

--- 2.cpp
+++ 2.cpp
@@ -1,37 +1,31 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include "stdlib.h"
 #include <conio.h>
 #include <string.h>
 #include <Windows.h>
-void input_passenger(Passenger person);
-void output_passenger(Passenger person);
 
-// Пассажир
 typedef struct {
 	char name[20];
 	int balance;
 } Passenger;
 
-// Автомобиль
 typedef struct {
 	char brand[20];
 	bool rate;
 	Fuel fuel;
 } Car;
 
-// Топливный бак
 typedef struct {
 	bool capacity;
 } Fuel;
 
-// Водитель
 typedef struct {
 	char name[20];
 	Car car;
 	int experience, orderAmount, balance;
 } Driver;
 
-// Заказ
 typedef struct {
 	bool rate, congestion;
 	Driver driver;
@@ -39,27 +33,54 @@ typedef struct {
 	int payment;
 } Order;
 
-// Ввод данных о пассажире
-void input_passenger(Passenger person) {
-	printf("** Ввод данных пассажира **\n");
-	do {
-		printf("Введите имя: ");
-		gets_s(person.name);
-	} while (person.name[0] == NULL);
-	do {
-		printf("Введите баланс: ");
-		while (scanf("%d", &person.balance) != 1) {
-			while (getchar() != '\n');
-			printf("Ошибка. Введите баланс: ");
-		}
-	} while (person.balance < 0);
-	printf("Данные успешно введены!\n\n");
+// Пассажир
+Passenger passenger_new(void);
+void set_passenger_name(Passenger* person, char* name);
+void set_passenger_balance(Passenger* person, int balance);
+const char* get_passenger_name(Passenger person);
+int get_passenger_balance(Passenger person);
+void input_car(Car car);
+
+Passenger passenger_new(void) {
+	Passenger passenger;
+	strcpy(passenger.name, "");
+	passenger.balance = 0;
+	return passenger;
 }
 
-// Вывод данных о пассажире
+void set_passenger_name(Passenger* person, char* name) {
+	strcpy(person->name, name);
+}
+
+void set_passenger_balance(Passenger* person, int balance) {
+	person->balance = balance;
+}
+
+const char* get_passenger_name(Passenger person) {
+	return person.name;
+}
+
+int get_passenger_balance(Passenger person) {
+	return person.balance;
+}
+
 void output_passenger(Passenger person) {
 	if (person.name[0] == NULL || person.balance < 0)
 		printf("Данные о пассажире отсутствуют!");
 	else
 		printf("Данные о пассажире:\n-Имя: %s\n-Баланс: %d\n\n", person.name, person.balance);
 }
+
+// Автомобиль
+
+void input_car(Car car) {
+	printf("** Ввод данных автомобиля **\n");
+	do {
+		printf("Введите марку: ");
+		gets_s(car.brand);
+	} while (car.brand[0] == NULL);
+}
+
+// Топливный бак
+// Водитель
+// Заказ
