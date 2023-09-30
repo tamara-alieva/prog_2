2f975d6cb763e5c4eba62cff38dd2e19329a4b72
Tamara <tamaraavgust14@gmail.com>
2023-09-30 13:18:19 +0700

    Замена struct на typedef struct. Добавление функции input_passenger. Добавление поля с именем в структуры

--- 2.cpp
+++ 2.cpp
@@ -3,28 +3,47 @@
 #include <conio.h>
 #include <string.h>
 #include <Windows.h>
+void input_passenger(Passenger person);
 
-struct Passenger {
+typedef struct {
+	char name[20];
 	int balance;
-};
+} Passenger;
 
-struct Car {
+typedef struct {
+	char brand[20];
 	bool rate;
 	Fuel fuel;
-};
+} Car;
 
-struct Fuel {
+typedef struct {
 	bool capacity;
-};
+} Fuel;
 
-struct Driver {
+typedef struct {
+	char name[20];
 	Car car;
 	int experience, orderAmount, balance;
-};
+} Driver;
 
-struct Order {
+typedef struct {
 	bool rate, congestion;
 	Driver driver;
 	Passenger passenger;
 	int payment;
-};
+} Order;
+
+void input_passenger(Passenger person) {
+	printf("** Ввод данных пассажира **\n");
+	do {
+		printf("Введите имя: ");
+		gets_s(person.name);
+	} while (person.name[0] == NULL);
+	do {
+		printf("Введите баланс: ");
+		while (scanf("%d", &person.balance) != 1) {
+			while (getchar() != '\n');
+			printf("Ошибка. Введите баланс: ");
+		}
+	} while (person.balance < 0);
+}
