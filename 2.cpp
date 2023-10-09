5984780efee3483cd1bd3f58f734b860aaff0a11
Tamara <tamaraavgust14@gmail.com>
2023-10-09 21:01:13 +0700

    Добавление класса Driver и его методов

--- 2.cpp
+++ 2.cpp
@@ -19,7 +19,7 @@ bool nameCheck(string name) {
 	return f;
 }
 
-bool balanceCheck(string balance) {
+bool intCheck(string balance) {
 	int i = 0; bool f = 1;
 	for (i = 0; i < balance.length(); i++) {
 		if (balance[i] >= '0' && balance[i] <= '9') f = 1;
@@ -74,7 +74,7 @@ public:
 		do {
 			cout << "Введите баланс: ";
 			cin >> temp;
-		} while (temp.empty() || balanceCheck(temp) == 0);
+		} while (temp.empty() || intCheck(temp) == 0);
 		this->balance = stoi(temp);
 		cout << "Данные успешно введены!" << endl << endl;
 	}
@@ -129,5 +129,68 @@ public:
 	void takePayment(int payment) {
 		int old_balance = this->getBalance();
 		this->setBalance(old_balance - payment);
+	}
+};
+
+class Driver : public Person {
+private:
+	int experience, orderAmount;
+public:
+	Driver() : Person() {
+		this->experience = 0;
+		this->orderAmount = 0;
+	}
+	Driver(string name) : Person(name) {
+		this->experience = 0;
+		this->orderAmount = 0;
+	}
+	Driver(int balance) : Person(balance) {
+		this->experience = 0;
+		this->orderAmount = 0;
+	}
+	Driver(string name, int balance, int experience, int orderAmount) : Person(name, balance) {
+		this->experience = experience;
+		this->orderAmount = orderAmount;
+	}
+	void setExperience(int experience) {
+		this->experience = experience;
+	}
+	void setOrderAmount(int orderAmount) {
+		this->orderAmount = orderAmount;
+	}
+	int getExperience() {
+		return this->experience;
+	}
+	int getOrderAmount() {
+		return this->orderAmount;
+	}
+	void input() {
+		string temp;
+		cout << "** Ввод данных водителя **" << this->getName() << endl;
+		do {
+			cout << "Введите количество лет опыта: ";
+			cin >> temp;
+		} while (temp.empty() || intCheck(temp) == 0);
+		this->experience = stoi(temp);
+		do {
+			cout << "Введите количество выполненных заказов: ";
+			cin >> temp;
+		} while (temp.empty() || intCheck(temp) == 0);
+		this->orderAmount = stoi(temp);
+		cout << "Данные успешно введены!" << endl << endl;
+	}
+	void output() {
+		cout << "Данные о водителе:" << endl << "-Имя: " << this->getName() << endl;
+		cout << "-Баланс: " << this->getBalance() << endl;
+		cout << "-Количество лет опыта: " << this->getExperience() << endl;
+		cout << "-Количество выполненных заказов: " << this->getOrderAmount() << endl << endl;
+	}
+	void givePayment(int payment) {
+		int old_balance = this->getBalance();
+		this->setBalance(old_balance + payment);
+	}
+	void increaseOrderAmount() {
+		int old_amount = this->getOrderAmount();
+		this->setOrderAmount(old_amount + 1);
 	}
 };
