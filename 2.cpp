34a0b30fab875ecc5fe0995f8ed65c04ce1f2380
Tamara <tamaraavgust14@gmail.com>
2023-10-09 21:49:45 +0700

    Добавление класса Car и его методов

--- 2.cpp
+++ 2.cpp
@@ -235,3 +235,68 @@ public:
 		this->capacity = 0;
 	}
 };
+
+class Car {
+private:
+	Fuel fuel;
+	string brand;
+	bool rate;
+public:
+	Car() {
+		this->fuel = new Fuel();
+		this->brand = "";
+		this->rate = 0;
+	}
+	Car(string brand) {
+		this->fuel = new Fuel();
+		this->brand = brand;
+		this->rate = 0;
+	}
+	Car(bool rate) {
+		this->fuel = new Fuel();
+		this->brand = "";
+		this->rate = rate;
+	}
+	Car(string brand, bool rate) {
+		this->fuel = new Fuel();
+		this->brand = brand;
+		this->rate = rate;
+	}
+	void setBrand(string brand) {
+		this->brand = brand;
+	}
+	void setRate(bool rate) {
+		this->rate = rate;
+	}
+	string getBrand() {
+		return this->brand;
+	}
+	bool getRate() {
+		return this->rate;
+	}
+	void input() {
+		string temp;
+		cout << "** Ввод данных автомобиля **" << endl;
+		do {
+			cout << "Введите марку: ";
+			cin >> this->brand;
+		} while (this->brand.empty() || nameCheck(this->brand) == 0);
+		do {
+			cout << "Введите класс автомобиля (0 - Эконом, 1 - Комфорт): ";
+			cin >> temp;
+		} while (temp != "0" && temp != "1");
+		this->rate = stoi(temp);
+		cout << "Данные успешно введены!" << endl << endl;
+	}
+	void output() {
+		if (this->brand.empty())
+			cout << "Данные об автомобиле отсутствуют!" << endl;
+		else {
+			cout << "Данные об автомобиле:" << endl << "-Марка: " << this->brand << endl << "-Класс: ";
+			if (this->rate)
+				cout << "Комфорт" << endl;
+			else
+				cout << "Эконом" << endl;
+		}
+	}
+};
